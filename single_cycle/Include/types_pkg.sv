2a356a68ddd8b618622cd92d92f1dead336ce9ac
Aya Tarek <ayatarek18917@gmail.com>
2025-07-15 02:58:10 +0300

    Controller: updated include file

--- single_cycle/Include/types_pkg.sv
+++ single_cycle/Include/types_pkg.sv
@@ -8,4 +8,18 @@ package types_pkg;
   typedef logic [XLEN-1:0] word_t;
   typedef logic [4:0]      reg_addr_t;  // 32 registers
 
+  // ALU control signals
+  typedef enum logic [3:0] {
+    ADD = 4'b0000,
+    SUB = 4'b0001,
+    AND = 4'b0010,
+    OR  = 4'b0011,
+    XOR = 4'b0100,
+    SRL = 4'b0110,
+    SRA = 4'b0111,
+    SLL = 4'b0101,
+    SLT = 4'b1000,
+    SLTU = 4'b1001
+  } alu_control_t;
+
 endpackage
