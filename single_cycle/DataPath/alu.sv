e9c7baa20f0bc134c73f091e9452df80e19ee437
AshrafByte <ahmed.ashraf2827@gmail.com>
2025-07-19 11:25:29 +0300

    Fix signed addition problem

--- single_cycle/DataPath/alu.sv
+++ single_cycle/DataPath/alu.sv
@@ -10,21 +10,21 @@ module alu (
     output logic    zero
 );
 
-    signed_word_t signed_a, signed_b;
-    assign signed_a = a;
-    assign signed_b = b;
+    signed_word_t sa, sb;
+    assign sa = a;
+    assign sb = b;
 
     always_comb begin
         unique case (control)
-            ALU_ADD:   result = a + b;
-            ALU_SUB:   result = a - b;
+            ALU_ADD:   result = sa + sb;
+            ALU_SUB:   result = sa - sb;
             ALU_AND:   result = a & b;
             ALU_OR:    result = a | b;
             ALU_XOR:   result = a ^ b;
             ALU_SRL:   result = a >> b[SHIFT_AMOUNT-1:0];
-            ALU_SRA:   result = signed_a >>> b[SHIFT_AMOUNT-1:0];
+            ALU_SRA:   result = sa >>> b[SHIFT_AMOUNT-1:0];
             ALU_SLL:   result = a << b[SHIFT_AMOUNT-1:0];
-            ALU_SLT:   result = (signed_a < signed_b) ? 1 : 0;
+            ALU_SLT:   result = (sa < sb) ? 1 : 0;
             ALU_SLTU:  result = (a < b) ? 1 : 0;
             default:   result = '0;
         endcase
