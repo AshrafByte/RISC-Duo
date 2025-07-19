bae42edeb148726920231b48bce686dc3c98aaf5
Aya Tarek <ayatarek18917@gmail.com>
2025-07-19 23:28:33 +0300

    fixed the shift in datapath

--- single_cycle/DataPath/Datapath.sv
+++ single_cycle/DataPath/Datapath.sv
@@ -46,7 +46,11 @@ module DataPath (
     // ==================================================
     // Sign-extension of raw immediates
     // ==================================================
-    assign imm_mux_in[IMMSRC_I] = {{20{imm_i_raw[11]}}, imm_i_raw};
+    Extension #(.WIDTI_IN(12), .WIDTI_OUT(32)) imm_ext (
+        .in(imm_i_raw),
+        .shift(ALUControl.shift),
+        .out(imm_mux_in[IMMSRC_I])
+    );
     assign imm_mux_in[IMMSRC_S] = {{20{imm_s_raw[11]}}, imm_s_raw};
     assign imm_mux_in[IMMSRC_B] = {{19{imm_b_raw[12]}}, imm_b_raw};
     assign imm_mux_in[IMMSRC_J] = {{11{imm_j_raw[20]}}, imm_j_raw};
